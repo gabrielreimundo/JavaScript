2b31f109675ee5688ec1e7a11f4704e63cbd60c9
Gabriel Reimundo <109987851+gabrielreimundo@users.noreply.github.com>
2022-11-24 11:22:56 -0300

    at

--- exercicios/ex007/script.js
+++ exercicios/ex007/script.js
@@ -3,16 +3,20 @@ function carregar(){
     var img = document.getElementById('imagem')
     var data = new Date()
     var hora = data.getHours()
+    //var hora = 
     msg.innerHTML = 'Agora são '+hora+' horas'
     if (hora >= 0 && hora <12) {
         //bomdia
         img.src = 'imagens/MANHÃ,café,bolacha,chocolate.jpg'
+        document.body.style.background = 'rgb(137, 66, 41)'
     } else if (hora >= 12 && hora < 18) {
         //boa tarde
         img.src = 'imagens/TARDE,gatito,sol.jpg'
+        document.body.style.background = 'rgb(214, 148, 75)'
     } else {
         //boa noite
         img.src ='imagens/NOITE.jpg'
+        document.body.style.background = 'rgb(17, 73, 78)'
     }
 }
 //rgb(214, 148, 75) cor tarde
