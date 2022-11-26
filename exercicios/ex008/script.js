5c53e55e81febeadfb243f13dbd8fb23534fadf6
Gabriel Reimundo <109987851+gabrielreimundo@users.noreply.github.com>
2022-11-26 20:54:03 -0300

    at

--- exercicios/ex008/script.js
+++ exercicios/ex008/script.js
@@ -24,21 +24,26 @@ function verificar() {
                 img.setAttribute('src', 'imagens/homemAdulto.jpg' )
             } else {
                 //idoso
-                img.setAttribute('src', 'imagens/homemAdulto.jpg' )
+                img.setAttribute('src', 'imagens/veio.jpg' )
             }
         } else if (fsex[1].checked) {
             genero = 'Mulher'
             if (idade >= 0 && idade < 12){
                 //criança
+                img.setAttribute('src', 'imagens/garotaCriança.jpg')
             } else if (idade < 21) {
                 //jovem
+                img.setAttribute('src', 'imagens/garotaAdolecente.jpg')
             } else if (idade < 50) {
                 //adulto
+                img.setAttribute('src', 'imagens/mulherAdulto.jpg' )
             } else {
                 //idoso
+                img.setAttribute('src', 'imagens/veia.jpg' )
             }
         }
         res.style.textAling = 'center'
         res.innerHTML = 'É '+genero+' com '+idade+' anos'
+        res.appendChild(img)
     }
 }
