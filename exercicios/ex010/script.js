89296c2c44c9e1dc75d4855e0907a131bcb10e00
Gabriel Reimundo <109987851+gabrielreimundo@users.noreply.github.com>
2022-12-14 21:56:17 -0300

    at

--- exercicios/ex010/script.js
+++ exercicios/ex010/script.js
@@ -6,14 +6,26 @@ function contar(){
 
     if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
       window.alert('[ERRO] Faltam dados !')
+      res.innerHTML = 'Impossivel contar'
     } else {
         res.innerHTML = 'Contando: '
         let i = Number(ini.value)
         let f = Number(fim.value)
         let p = Number(passo.value)
-
-        for(let c = i; c <= f; c += p) {
-            res.innerHTML += ` ${c} \u{1F633}	`
+        if(p <= 0) {
+            window.alert('Passo invalido! Considerando passo 1')
+            p = 1
+        }
+        if(i < f){
+            // contagem crescente
+            for(let c = i; c <= f; c += p) {
+                res.innerHTML += ` ${c} \u{1F633}	`
+            }
+        } else {
+            //contagem regressiva
+            for(let c = i; c >= f; c -= p){
+                res.innerHTML += ` ${c} \u{1F633}	`
+            }
         }
     }
 }
