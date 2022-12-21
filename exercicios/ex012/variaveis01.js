b313b880b10261f9d37abcfc194484974faf589c
Gabriel Reimundo <109987851+gabrielreimundo@users.noreply.github.com>
2022-12-21 20:49:43 -0300

    at

---
+++ exercicios/ex012/variaveis01.js
@@ -0,0 +1,11 @@
+// Variaveis simples so armazenam um valor por vez
+// Variaveis compostas são capazes de armazenar varios valores 
+// Variaveis compostas sempre começam com o indice 0
+
+let num = [5, 6, 7, 8 ]
+console.log(`Nosso vetor é o ${num}`)
+num.push(3)//para adicionar valor
+console.log(`vetor ${num}`)
+console.log(`Nosso vetor é o ${num.length}`)//.length para ver a quantidade de posições
+console.log(`Nosso vetor é o ${num.sort()}`)//.sort para colocar em ordem crescente
+console.log(`Nosso vetor é o ${num[3]}`) //para mostrar algum valor de uma posição especifica
---
+++ exercicios/ex012/variaveis02.js
@@ -0,0 +1,5 @@
+let valores = [2, 5, 8, 6, 9, 7]
+
+for(let pos = 0; pos < valores.length; pos++){
+    console.log(`A posição ${pos} tem o valor ${valores[pos]}`)
+}
