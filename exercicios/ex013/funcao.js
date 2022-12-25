754871c349bd22785ff2fffbc6dea2543c1db322
Gabriel Reimundo <109987851+gabrielreimundo@users.noreply.github.com>
2022-12-24 21:28:12 -0300

    at

--- exercicios/ex013/funcao.js
+++ exercicios/ex013/funcao.js
@@ -1,2 +1,12 @@
 // Funções são ações executadas assim que são chamadas ou em decorrencia de algum evento. 
 // Funções podem receber parametros e retornar um resultado.
+
+function parimpar(n) {
+    if (n%2 == 0){
+        return 'Par!'
+    } else {
+        return 'Impar!'
+    }
+}
+
+console.log(parimpar(8))
